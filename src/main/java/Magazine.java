9ce7f4ffb8ac4a403e707f4b8ee8b45ac64b0e56
Notaniell <tok_pv@mail.ru>
2026-01-06 02:55:50 +0400

    Add added bullets in magazine.

--- src/main/java/Magazine.java
+++ src/main/java/Magazine.java
@@ -9,7 +9,8 @@ public class Magazine {
         boolean loaded = false;
 
         while(!loaded) {
-            System.out.println("Сколько патронов заряжаем? (0-6)");
+            System.out.println("Сколько патронов заряжаем? Сейчас в магазине " + currentBullet +".\n" +
+                    "Максимум 6. Можешь добавить " + (MAXBULLET-currentBullet));
             if (!scanner.hasNextInt()) {
                 System.out.println("Ошибка: нужно ввести целое число!");
                 scanner.next();
@@ -17,10 +18,10 @@ public class Magazine {
                 int input = scanner.nextInt();
                 if (input < 0) {
                     System.out.println("Патроны не могут быть отрицательными!");
-                } else if (input > MAXBULLET) {
+                } else if (input + currentBullet > MAXBULLET) {
                     System.out.println("Слышь у нас шестизарядный револьвер, понимаешь 6 патронов максимум!");
                 } else {
-                    currentBullet = input;
+                    currentBullet = currentBullet + input;
                     System.out.println("Вы зарядили патронов: " + input);
                     loaded = true;
                 }
